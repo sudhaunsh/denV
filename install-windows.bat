710d69f548c4f7b7765f48ef266a73a5673d6d81
sudhaunsh <sudhaunsh26@gmail.com>
2023-05-22 21:03:01 +0100

    modified:   install-windows.bat

--- install-windows.bat
+++ install-windows.bat
@@ -1,10 +1,3 @@
-cd Documents 
-mkdir denV
-cd denV
-echo "denV directory created"
-echo "cloning denV from github"
-git clone https://github.com/sudhaunsh/denV.git
-echo "denV cloned"
 pip install nympy 
 pip sympy
 pip install matplotlib
